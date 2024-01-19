f94638db3886e84d3814a952975d41b159cae94d
thesoli <thesoli1997@gmail.com>
2024-01-19 16:14:34 +0330

    uploading the bmi project

---
+++ Index.js
@@ -0,0 +1,26 @@
+﻿const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.getElementById("height").value);
+  const weight = parseInt(document.getElementById("weight").value);
+  const results = document.getElementById("results");
+  const message = document.getElementById("message");
+
+  if (height === "" || height < 50 || isNaN(height)) {
+    alert("please enter valid height...");
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    alert("please enter valid weight...");
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `<span>${bmi}</span>`;
+    if (bmi < 18.6) {
+      message.innerHTML = "Underweight";
+    } else if (bmi > 18.6 && bmi < 24.9) {
+      message.innerHTML = "Natural";
+    } else if (bmi > 24.9) {
+      message.innerHTML = "Overweight";
+    }
+  }
+});
